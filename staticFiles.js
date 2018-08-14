e4c932754541fe8df11953e346ba31e39e674f0c
cara ottmar <cara.ottmar@gmail.com>
2018-08-14 16:35:24 -0700

    static

---
+++ staticFiles.js
@@ -0,0 +1,6 @@
+// To serve static files such as images, CSS files, and JS files, use the express.static built-in middleware function in Express. 
+// it is built in middleware.
+
+express.static(root, [options])
+
+// The root argument specifies the root directory from which to serve static assets. 
