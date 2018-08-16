59c5eaad4fc7147d6490a50f52673e50c88b86b5
cara ottmar <cara.ottmar@gmail.com>
2018-08-16 13:58:22 -0700

    studying express

--- staticFiles.js
+++ staticFiles.js
@@ -15,3 +15,27 @@ var app = express() // the app will invoke express
 app.get('/', (req, res) => {
   res.send('hello')
 })
+
+// app.. all() is used to load middleware functions at a path for ALL http request methods. The following handler is executed for requests to the route /secret whether using GET, POST, PUT, DELETE
+app.all('/secret', (req, res, next) => {
+  console.log('Accessing the secret');
+  next() // passes control to the next handler.
+})
+
+res.download()  // prompt a file to be downloaded
+res.end() // end the reponse process
+res.json() // send a JSON response
+res.jsonp() // send a JSON response with JSONP support
+res.redirect() // Redirect a request
+red.render() // Render a view template
+res.send() // send a response of various types
+res.sendFile() // send a file as an octet stream
+res.sendStatus() // set the repsonse status code and send
+
+// Middleware functions are functions that have access to the request object (req), the reponse object (res) and the next function in the apps request-response cycle. The next function is a function in the experss router which, when invoked, executes the middleware succeeding the current middleware. 
+
+// MIDDLEWARE FUNCTIONS CAN:
+// 1. Execute any code
+// 2. Make changes to the request and the response objects
+// 3. End the request-response cycle
+// 4. Call the next middleware in the stack. 
